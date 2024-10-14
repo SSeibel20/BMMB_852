b2991c895cf9774ced199d5351c4bb79aa1d96fd
sls6550@psu.edu <Samantha Seibel>
2024-10-14 01:02:25 -0400

    Update HW5.sh

--- HW5/HW5.sh
+++ HW5/HW5.sh
@@ -12,8 +12,8 @@ micromamba activate bioinfo
 
 #Set Paths for reproducibility
 #Change the variables to suit your needs
-HOME_DIR="/Users/sls6550/work/BMMB_852/HW5"
-FASTA="ecoli_0157_h7ncbi.fasta"
+HOME_DIR="~/work/BMMB_852/HW5"
+FASTA="Salmonella_Typhimurium.fasta"
 
 # Move to the desired directory to download your files
 cd $HOME_DIR
@@ -31,19 +31,19 @@ grep -c ">" $FASTA
 seqtk comp $FASTA
 
 #Generate a simulated FASTQ output for a sequencing instrument of your choice.  Set the parameters so that your target coverage is 10x**
-wgsim -N 366667 -1 150 -2 150 $FASTA sim_ecoli1.fastq sim_ecoli2.fastq
+wgsim -N 366667 -1 150 -2 150 $FASTA sim_1.fastq sim_2.fastq
 
 #Number of reads generated
-cat sim_ecoli1.fastq | wc -l
-cat sim_ecoli2.fastq | wc -l
+cat sim_1.fastq | wc -l
+cat sim_2.fastq | wc -l
 
 #Average read length
 grep -A 2 '^@' *.fastq | grep -v '^@' | awk '{count += length($0); total++} END {print count / total}'
 
 #Size of the FASTQ files
-ls -lh sim_ecoli1.fastq
-ls -lh sim_ecoli2.fastq
+ls -lh sim_1.fastq
+ls -lh sim_2.fastq
 
 #Compress the files and report size
-gzip sim_ecoli1.fastq sim_ecoli2.fastq
-ls -lh sim_ecoli1.fastq.gz sim_ecoli2.fastq.gz
+gzip sim_1.fastq sim_2.fastq
+ls -lh sim_1.fastq.gz sim_2.fastq.gz
